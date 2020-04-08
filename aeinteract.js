2a7df568824af403119bceca41c0f7e5d8c0f797
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2020-04-08 15:57:20 +0530

    Added ImageLayers and other properties

--- aeinteract.js
+++ aeinteract.js
@@ -6,29 +6,46 @@ ae.options.includes = [
   "./node_modules/after-effects/lib/includes/es5-shim.jsx",
   "./node_modules/after-effects/lib/includes/get.jsx",
 ];
+
+// ae.options.program = path.join('OtherAppDirectory','Adobe After Effects 2015');
+
 /**
  * @param  {String} filePath - path of aep or aepx file
  */
-
 const getStructure = (filePath) =>
-  ae.execute((fp) => {
-    // don't open file if already loaded
-    if (!(app.project.file && app.project.file.toString().includes(fp)))
-      app.open(new File(fp));
+  ae
+    .execute((fp) => {
+      // don't open file if already loaded
+      if (!(app.project.file && app.project.file.toString().includes(fp)))
+        app.open(new File(fp));
 
-    const comps = [];
-    const textLayers = [];
+      const comps = [];
+      const textLayers = [];
+      const imageLayers = [];
 
-    get(CompItem).each((x) => comps.push(x.name));
-    get(TextLayer).each((t) => textLayers.push(t.name));
+      get(CompItem).each((x) => comps.push(x.name.trim()));
+      get(TextLayer).each((t) => {
+        const ti = {};
+        ti["name"] = t.name.trim();
+        ti["text"] = t.property("Source Text").value.toString().trim();
+        console.log(ti.name, ti.text);
+        textLayers.push(ti);
+      });
+      get(FootageItem).each((f) => {
+        const fi = {};
+        if (f.mainSource.isStill) {
+          fi["name"] = f.name.trim();
+          fi["height"] = f.height;
+          fi["width"] = f.width;
+          imageLayers.push(fi);
+        }
+      });
 
-    return { comps, textLayers };
-  }, filePath);
+      //purge memory
+      app.purge(PurgeTarget.ALL_CACHES);
+      return { comps, textLayers, imageLayers };
+    }, filePath)
+    .catch(console.log);
 
-const sandBox = async (fp) => {
-  return ae.execute((fp) => {
-    return console.log(app.project.file.toString());
-  }, fp);
-};
 getStructure("~/Desktop/myFile.aep").then(console.log).catch(console.error);
 module.exports = { getStructure };
