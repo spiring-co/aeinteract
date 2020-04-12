d566803db0de0df77e5580c65de4a1de5cbd23e4
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2020-04-12 16:34:14 +0530

    Added renderer

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const fileUpload = require("express-fileupload");
 const ae = require("./aeinteract");
+const { renderFromData } = require("./renderer");
+var bodyParser = require("body-parser");
 const port = 4488;
 
 const fileUploadConfig = {
@@ -14,25 +17,38 @@ const fileUploadConfig = {
   limits: { fileSize: 50 * 1024 * 1024 },
 };
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(fileUpload(fileUploadConfig));
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => res.json({ message: "API Working" }));
 
 //expects file object in aepFile
 // set a high timeout in client as loading a big project may take long time
 //TODO restrict file types
-app.post("/getStructureFromFile", async (req, res) => {
+app.post("/getStructureFromFile", async (req, res, next) => {
   try {
     if (!req.files || !Object.keys(req.files).length) {
       return res.status(400).json({ message: "No files were uploaded." });
     }
-
+    req.files.aepFile.mv(`./public/templates/${req.files.aepFile.name}`);
+    console.log(`${__dirname}/public/templates/${req.files.aepFile.name}`);
     return res.json({
-      data: await ae.getStructure(req.files.aepFile.tempFilePath),
+      data: await ae.getProjectStructure(
+        `${__dirname}/public/templates/${req.files.aepFile.name}`
+      ),
+      fileUrl: `${getUrl(req)}/templates/${req.files.aepFile.name}`,
     });
   } catch (err) {
     next(err);
   }
+});
+
+app.post("/render", async (req, res) => {
+  if (!req.body) return res.status(400).json({ message: "No body provided" });
+  renderFromData(req.body);
+  res.json({ message: "Rendering job." });
 });
 
 app.use((err, req, res, next) => {
@@ -41,3 +57,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => console.log(`Running at http://localhost:${port}`));
+
+const getUrl = (req) => `${req.protocol}://${req.headers.host}`;
